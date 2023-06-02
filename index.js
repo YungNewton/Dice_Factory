5459fae7d67a6cea3656a921f2c826d4c29e0a2f
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-02 16:35:33 +0100

    commit

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ function diceWinner(){
     var diceOne = Math.floor(Math.random() * 6);
     var diceTwo = Math.floor(Math.random() * 6);
     if (diceOne > diceTwo){
-        document.querySelector("body > div  p").classList.add("player_one");
-        document.querySelector("body > div  p").textContent = "Player One Wins";
+        document.querySelector("body > div  p").innerHTML = "<img src = 'https://cdn-icons-png.flaticon.com/512/4481/4481086.png' height = '70px'/> Hurray Player One Won.";
     }else if(diceTwo > diceOne){
-        document.querySelector("body > div  p").classList.add("player_two");
-        document.querySelector("body > div  p").textContent = "Player Two Wins";
+        document.querySelector("body > div  p").innerHTML = "<img src = 'https://cdn-icons-png.flaticon.com/512/4481/4481086.png' height = '70px'/> Hurray Player Two Won.";
     }else{
         document.querySelector("body > div  p").textContent = "Draw";
+    }
+    switch(diceOne){
+        case 1:
+            
     }
 }
 document.querySelector("button").onclick = function(){diceWinner()};
